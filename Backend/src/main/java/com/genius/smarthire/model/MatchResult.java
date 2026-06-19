4032c2f7e9a47d1eab7ae8468b9fbd094a3f8b33
Varshagupta143 <varshagupta2105@gmail.com>
2026-06-19 19:17:00 +0530

    fix main

--- Backend/src/main/java/com/genius/smarthire/model/MatchResult.java
+++ Backend/src/main/java/com/genius/smarthire/model/MatchResult.java
@@ -1,8 +1,10 @@
 package com.genius.smarthire.model;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+@Data
+@NoArgsConstructor
 @Document(collection = "match_results")
 public class MatchResult {
     @Id
