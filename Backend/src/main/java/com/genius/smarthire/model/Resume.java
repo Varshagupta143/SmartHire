865775635494489a220ca5d5e0300b59b9eb44e3
Varshagupta143 <varshagupta2105@gmail.com>
2026-06-19 18:39:33 +0530

    fix main

--- Backend/src/main/java/com/genius/smarthire/model/Resume.java
+++ Backend/src/main/java/com/genius/smarthire/model/Resume.java
@@ -2,7 +2,8 @@ package com.genius.smarthire.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 @Data
 @NoArgsConstructor
